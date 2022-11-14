d617a3148eb547503f82d5ed299bd695db510d32
jmaksimik <jmaks@me.com>
2022-11-14 13:22:30 -0500

    Update event listener function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
   /*----- constants -----*/
+  let gameBoard = [[0, 0, 0, 0], 
+                   [0, 0, 0, 0],  
+                   [0, 0, 0, 0], 
+                   [0, 0, 0, 0]];
 
-
-const randomStart = Math.floor(Math.random() * 2);
+const randomStart = Math.floor(Math.random() * 2) + 1;
 
 
   /*----- state variables -----*/
@@ -13,25 +16,29 @@ init();
 
 
 function init(){
-  const gameBoard = [[0, 0, 0, 0], 
+  let gameBoard = [[0, 0, 0, 0], 
                      [0, 0, 0, 0],  
                      [0, 0, 0, 0], 
                      [0, 0, 0, 0]];
   
   playerTurn = randomStart;
   console.log(playerTurn);
-  if (playerTurn === 0){
+  if (playerTurn === 1){
     console.log("Player One's Turn")
   } else{
     console.log("Player Two's Turn");
   };
-
+  render();
 };
 
 
   /*----- cached elements  -----*/
-  const buttonEl = document.querySelector('button');
-  const spanEl = document.querySelector('.slots')
+  const buttonEl = document.querySelector('#button1');
+  const spanEl = document.querySelector('.slots');
+  const div1El = document.querySelector('.column1');
+  const div2El = document.querySelector('.column2');
+  const div3El = document.querySelector('.column3');
+  const div4El = document.querySelector('.column4');
 
 
   /*----- event listeners -----*/
@@ -40,5 +47,27 @@ buttonEl.addEventListener('click', fillSlot);
 
   /*----- functions -----*/
   function fillSlot(e) {
-    spanEl.style.backgroundColor = 'red';
-  }
+    if(e.target.id === 'button1'){
+      for (let i = gameBoard.length - 1; i >= 0; i-- ) {
+        if (gameBoard[i][0] === 0){
+          gameBoard[i][0] = playerTurn; 
+          return console.log(gameBoard);
+        } else if(gameBoard[i][0].includes(!0)) {
+          console.log('No available slots');
+         } 
+      }; 
+    };
+    render();
+  };
+  
+
+function render(){
+  gameBoard.forEach(function(value, index) {
+    if(value === 1){
+      for(let i = 0; i < div1El.children.length - 1; i++){
+        div1El.children[i].classList.add('player-one');
+        console.log(div1El.children[i]);
+      }
+    }
+  })
+};
