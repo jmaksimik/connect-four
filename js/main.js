b7d8b4b1fdc430925a2c40aaefa0043af1da33bd
jmaksimik <jmaks@me.com>
2022-11-14 15:21:58 -0500

    Individually split out functions and event-listeners with fill logic

--- js/main.js
+++ js/main.js
@@ -28,12 +28,16 @@ function init(){
   } else{
     console.log("Player Two's Turn");
   };
-  render();
+
 };
 
 
   /*----- cached elements  -----*/
-  const buttonEl = document.querySelector('#button1');
+  const buttonEl1 = document.querySelector('#button1');
+  const buttonEl2 = document.querySelector('#button2');
+  const buttonEl3 = document.querySelector('#button3');
+  const buttonEl4 = document.querySelector('#button4');
+
   const spanEl = document.querySelector('.slots');
   const div1El = document.querySelector('.column1');
   const div2El = document.querySelector('.column2');
@@ -42,32 +46,62 @@ function init(){
 
 
   /*----- event listeners -----*/
-buttonEl.addEventListener('click', fillSlot);
+buttonEl1.addEventListener('click', fillColumn1);
+buttonEl2.addEventListener('click', fillColumn2);
+buttonEl3.addEventListener('click', fillColumn3);
+buttonEl4.addEventListener('click', fillColumn4);
 
 
   /*----- functions -----*/
-  function fillSlot(e) {
-    if(e.target.id === 'button1'){
-      for (let i = gameBoard.length - 1; i >= 0; i-- ) {
-        if (gameBoard[i][0] === 0){
-          gameBoard[i][0] = playerTurn; 
-          return console.log(gameBoard);
-        } else if(gameBoard[i][0].includes(!0)) {
-          console.log('No available slots');
-         } 
-      }; 
+  function fillColumn1() {
+    for (let i = gameBoard.length - 1; i >= 0; i-- ) {
+      if (gameBoard[i][0] === 0){
+        gameBoard[i][0] = playerTurn; 
+        console.log(gameBoard);
+        return;
+      };
+        };
+    if (gameBoard[0][0] !==0){
+      console.log('No available slots');
+      return;
     };
-    render();
   };
   
+  function fillColumn2() {
+    for (let i = gameBoard.length - 1; i >= 0; i-- ) {
+      if (gameBoard[i][1] === 0){
+        gameBoard[i][1] = playerTurn; 
+        return console.log(gameBoard);
+      };
+        };
+      if (gameBoard[0][1] !==0){
+      console.log('No available slots'); //will change to an HTML message at later date
+      return;
+    };
+  };
 
-function render(){
-  gameBoard.forEach(function(value, index) {
-    if(value === 1){
-      for(let i = 0; i < div1El.children.length - 1; i++){
-        div1El.children[i].classList.add('player-one');
-        console.log(div1El.children[i]);
-      }
-    }
-  })
-};
+  function fillColumn3() {
+    for (let i = gameBoard.length - 1; i >= 0; i-- ) {
+      if (gameBoard[i][2] === 0){
+        gameBoard[i][2] = playerTurn; 
+        return console.log(gameBoard);
+      };
+        };
+      if (gameBoard[0][2] !==0){
+      console.log('No available slots'); //will change to an an HTML message at later date
+      return;
+    };
+  };
+
+  function fillColumn4() {
+    for (let i = gameBoard.length - 1; i >= 0; i-- ) {
+      if (gameBoard[i][3] === 0){
+        gameBoard[i][3] = playerTurn; 
+        return console.log(gameBoard);
+      };
+        };
+      if (gameBoard[0][3] !==0){
+      console.log('No available slots'); //will change to an an HTML message at later date
+      return;
+    };
+  };
