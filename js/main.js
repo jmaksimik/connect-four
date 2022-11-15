3235e8288aa54bfcede1aba0a9fff660a6586650
jmaksimik <jmaks@me.com>
2022-11-15 17:00:55 -0500

    Write diagonal win condition logic

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ buttonEl4.addEventListener('click', fillColumn4);
         gameBoard[i][0] = playerTurn; 
         if (playerTurn === 1){
           div1El.children[i].classList.add('player-one');
-          return render();
+          playerTurn = 2;
         } else {
           div1El.children[i].classList.add('player-two');
           playerTurn = 1;
@@ -79,8 +79,10 @@ buttonEl4.addEventListener('click', fillColumn4);
         gameBoard[i][1] = playerTurn; 
         if (playerTurn === 1){
           div2El.children[i].classList.add('player-one');
+          playerTurn = 2;
         } else {
           div2El.children[i].classList.add('player-two');
+          playerTurn = 1;
         }
         return render();
       };
@@ -97,8 +99,10 @@ buttonEl4.addEventListener('click', fillColumn4);
         gameBoard[i][2] = playerTurn; 
         if (playerTurn === 1){
           div3El.children[i].classList.add('player-one');
+          playerTurn = 2;
         } else {
           div3El.children[i].classList.add('player-two');
+          playerTurn = 1;
         }
         return render();
       };
@@ -115,8 +119,10 @@ buttonEl4.addEventListener('click', fillColumn4);
         gameBoard[i][3] = playerTurn; 
         if (playerTurn === 1){
           div4El.children[i].classList.add('player-one');
+          playerTurn = 2;
         } else {
           div4El.children[i].classList.add('player-two');
+          playerTurn = 1;
         }
         return render();
       };
@@ -128,13 +134,19 @@ buttonEl4.addEventListener('click', fillColumn4);
   };
 
   function matchChecker(w, x, y, z){
-    return ((w !==0) && (w === x) && (w === y) && (w === z));
-  }
-
+    if([w, x, y, z].every(e => e === 1)){
+      return [true, true];
+    }else if([w, x, y, z].every(e => e === 2)){
+      return [true, false];
+    } else{
+      return [false, null];
+    }
+  };
   function checkHorizontal() {
     gameBoard.forEach(function(arr) {
-      if(matchChecker(arr[0], arr[1], arr[2], arr[3])){
-        if(playerTurn === 1){
+      const matchWon = matchChecker(arr[0], arr[1], arr[2], arr[3]);
+      if(matchWon[0]){
+        if(matchWon[1]){
           buttonEl1.disabled = true;
           buttonEl2.disabled = true;
           buttonEl3.disabled = true;
@@ -154,18 +166,21 @@ buttonEl4.addEventListener('click', fillColumn4);
   function checkVertical() {
     for(let i = 0; i < gameBoard.length; i++){
       for(let j = 0; j < gameBoard[i].length; j++){
-        if(matchChecker(gameBoard[0][j], gameBoard[1][j], gameBoard[2][j], gameBoard[3][j])){
-          if(playerTurn === 1){
+        const matchWon = matchChecker(gameBoard[0][j], gameBoard[1][j], gameBoard[2][j], gameBoard[3][j]);
+        if(matchWon[0]){
+          if(matchWon[1]){
             buttonEl1.disabled = true;
             buttonEl2.disabled = true;
             buttonEl3.disabled = true;
             buttonEl4.disabled = true;
+            console.log('player 1 won')
             return winningPlayer = 1;
           } else {
             buttonEl1.disabled = true;
             buttonEl2.disabled = true;
             buttonEl3.disabled = true;
             buttonEl4.disabled = true;
+            console.log('player 2 won')
             return winningPlayer = 2;
           };
         };
@@ -174,28 +189,40 @@ buttonEl4.addEventListener('click', fillColumn4);
   };
 
   function checkDiagonal() {
-    for(let i = 0; i < gameBoard.length; i++){
-      for(let j = 0; j < gameBoard.length; j++){
-        if(matchChecker(gameBoard[0][j], gameBoard[1][j], gameBoard[2][j], gameBoard[3][j] || 
-                        gameBoard[0][j+3], gameBoard[1][j+1], gameBoard[2][j-1], gameBoard[3][j-3])){
-                          if(playerTurn === 1){
-                            buttonEl1.disabled = true;
-                            buttonEl2.disabled = true;
-                            buttonEl3.disabled = true;
-                            buttonEl4.disabled = true;
-                            return winningPlayer = 1;
-                          } else { 
-                            buttonEl1.disabled = true;
-                            buttonEl2.disabled = true;
-                            buttonEl3.disabled = true;
-                            buttonEl4.disabled = true;
-                            return winningPlayer = 2;
-                          };
+    let matchWon = matchChecker(gameBoard[0][0], gameBoard[1][1], gameBoard[2][2], gameBoard[3][3]);
+        if(matchWon[0]){
+          if(matchWon[1]){
+            buttonEl1.disabled = true;
+            buttonEl2.disabled = true;
+            buttonEl3.disabled = true;
+            buttonEl4.disabled = true;
+            return winningPlayer = 1;
+          } else { 
+            buttonEl1.disabled = true;
+            buttonEl2.disabled = true;
+            buttonEl3.disabled = true;
+            buttonEl4.disabled = true;
+            return winningPlayer = 2;
+                };
           };
+        matchWon = matchChecker(gameBoard[0][3], gameBoard[1][2], gameBoard[2][1], gameBoard[3][0]);
+      if(matchWon[0]){
+        if(matchWon[1]){
+        buttonEl1.disabled = true;
+        buttonEl2.disabled = true;
+        buttonEl3.disabled = true;
+        buttonEl4.disabled = true;
+        return winningPlayer = 1;
+      } else { 
+        buttonEl1.disabled = true;
+        buttonEl2.disabled = true;
+        buttonEl3.disabled = true;
+        buttonEl4.disabled = true;
+        return winningPlayer = 2;
       };
-    };
-  };
-    
+};
+
+        };
   function render() {
     
     announceEl.innerText = "";
@@ -209,21 +236,11 @@ buttonEl4.addEventListener('click', fillColumn4);
     checkVertical();
     checkDiagonal();
 
-    
-
-    // if(playerTurn === 1){
-    //   playerTurn = 2;
-    //   turnEl.innerText = "Player Two's Turn";
-    // } else {
-    //   playerTurn = 1;
-    //   turnEl.innerText = "Player One's Turn";
-    // }
-
     if(winningPlayer === 1){
       announceEl.innerText = "Player One Wins!";
       turnEl.innerText = "";
     } else if(winningPlayer === 2){
-      announceEl.innerText = "Player One Wins";
+      announceEl.innerText = "Player Two Wins";
       turnEl.innerText = "";
    
     }
